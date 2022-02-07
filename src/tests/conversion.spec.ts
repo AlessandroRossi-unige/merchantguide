12b705327db547b728746b6ae97d4d67e5140c90
Alessandro Rossi <S4641902@studenti.unige.it>
2022-02-07 19:15:16 +0100

    Added test for not recognised element

--- src/tests/conversion.spec.ts
+++ src/tests/conversion.spec.ts
@@ -1,5 +1,6 @@
 import {produceOutputFromNotes} from "../utils/conversion";
 import {Notes} from "../entities/Notes";
+import {UnknownValueError} from "../ErrorHandling/UnknownValueError";
 
 describe('Fetching input test suite', () => {
   test('Input is one translation of 3', () => {
@@ -28,6 +29,18 @@ describe('Fetching input test suite', () => {
     let output = produceOutputFromNotes(notes);
     expect(output.translationList).toEqual([[['vnvc', 'dxzc', 'pdlk'], 14]]);
     expect(output.conversionMap).toEqual([['Silver', ['vnvc', 'dxzc', 'dxzc', 'dxzc'], 27.53]]);
+  });
+  
+  test('Input is one translation of 14 and one conversion of 17 Zinc (Not recognised)', () => {
+    let notes: Notes;
+    notes = new Notes(new Map<string, string>([['dxzc', 'I'], ['pdlk', 'V'], ['vnvc', 'X']]),
+      new Map<string, string[]>([['Silver', ['vnvc', 'pdlk', 'dxzc', 'dxzc', '36']]]),
+      [['vnvc', 'dxzc', 'pdlk']], [(['Zinc', ['vnvc', 'dxzc','dxzc', 'dxzc']])]);
+    function generateSymbolFunc() {
+      produceOutputFromNotes(notes);
+    }
+    expect(generateSymbolFunc).toThrow(UnknownValueError);
+    expect(generateSymbolFunc).toThrow(`I don't have information on this: 'Zinc'`);
   });
   
  
